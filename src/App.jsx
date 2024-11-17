f348d30934ff8c383645125a913a9f4bc1e2a42c
johnasblasco <johnaslblasco@gmail.com>
2024-11-18 01:07:59 +0800

    adsad

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import ProductProvider from './onlineListing/page';
 
 // Pages
-import MainPage from './onlineListing/pages/ECommerce';
+import MainPage from './onlineListing/pages/Ecommerce';
 import StoreLocation from './onlineListing/pages/StoreLocation';
 import ContactUs from './onlineListing/pages/ContactUs';
 import Faq from './onlineListing/pages/Faq';
@@ -25,39 +25,39 @@ import ViewProducts from './onlineListing/components/ViewProducts';
 import Erceipt from './onlineListing/components/Erceipt';
 
 function App() {
-    console.log('ECommerce Import:', MainPage);
-    return (
-        <ProductProvider>
-            <Router>  {/* Make sure Router is wrapping the Routes */}
-                <Routes>
-                    {/* Main E-commerce Routes */}
-                    <Route path="/iRIG" element={<MainPage />} />
-                    <Route path="/Ereceipt" element={<Erceipt />} />
-                    <Route path="/iRIG/products/view/:id" element={<ViewProducts />} />
-                    
-                    {/* Utility and Miscellaneous Routes */}
-                    <Route path="*" element={<Navigate to="/iRIG" />} />
-                    
-                    {/* Informational Pages */}
-                    <Route path="/iRIG/our-store" element={<StoreLocation />} />
-                    <Route path="/iRIG/contact-us" element={<ContactUs />} />
-                    <Route path="/iRIG/warranty" element={<Warranty />} />
-                    <Route path="/iRIG/return-policy" element={<ReturnPolicy />} />
-                    <Route path="/iRIG/faq" element={<Faq />} />
-                    
-                    {/* Product Pages */}
-                    <Route path="/iRIG/cart" element={<CartPage />} />
-                    <Route path="/iRIG/laptops" element={<Laptops />} />
-                    <Route path="/iRIG/desktops" element={<Desktops />} />
-                    <Route path="/iRIG/accessories" element={<Accessories />} />
-                    <Route path="/iRIG/peripherals" element={<Peripherals />} />
-                    <Route path="/iRIG/pcfurniture" element={<PCFurniture />} />
-                    <Route path="/iRIG/osandsoftware" element={<OSAndSoftware />} />
-                    <Route path="/iRIG/components" element={<Components />} />
-                </Routes>
-            </Router>
-        </ProductProvider>
-    );
+      console.log('ECommerce Import:', MainPage);
+      return (
+            <ProductProvider>
+                  <Router>  {/* Make sure Router is wrapping the Routes */}
+                        <Routes>
+                              {/* Main E-commerce Routes */}
+                              <Route path="/iRIG" element={<MainPage />} />
+                              <Route path="/Ereceipt" element={<Erceipt />} />
+                              <Route path="/iRIG/products/view/:id" element={<ViewProducts />} />
+
+                              {/* Utility and Miscellaneous Routes */}
+                              <Route path="*" element={<Navigate to="/iRIG" />} />
+
+                              {/* Informational Pages */}
+                              <Route path="/iRIG/our-store" element={<StoreLocation />} />
+                              <Route path="/iRIG/contact-us" element={<ContactUs />} />
+                              <Route path="/iRIG/warranty" element={<Warranty />} />
+                              <Route path="/iRIG/return-policy" element={<ReturnPolicy />} />
+                              <Route path="/iRIG/faq" element={<Faq />} />
+
+                              {/* Product Pages */}
+                              <Route path="/iRIG/cart" element={<CartPage />} />
+                              <Route path="/iRIG/laptops" element={<Laptops />} />
+                              <Route path="/iRIG/desktops" element={<Desktops />} />
+                              <Route path="/iRIG/accessories" element={<Accessories />} />
+                              <Route path="/iRIG/peripherals" element={<Peripherals />} />
+                              <Route path="/iRIG/pcfurniture" element={<PCFurniture />} />
+                              <Route path="/iRIG/osandsoftware" element={<OSAndSoftware />} />
+                              <Route path="/iRIG/components" element={<Components />} />
+                        </Routes>
+                  </Router>
+            </ProductProvider>
+      );
 }
 
 export default App;
